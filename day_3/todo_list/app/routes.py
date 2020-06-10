2c9d70db27d3abbf539c719e709590b3fd2d924a
Stephen Ramsay <rabrandom@gmail.com>
2020-06-10 14:27:11 +0100

    more code

--- day_3/todo_list/app/routes.py
+++ day_3/todo_list/app/routes.py
@@ -1,5 +1,25 @@
 from app import app
+from flask import render_template
+
+@app.route('/<name>')
+def index(name):
+    return f"{name} loves cake!"
 
 @app.route('/')
-def index():
-    return "Hello World!"
+def home():
+    user = {"username": "Marina"}
+    tasks = [
+    {
+        'id': 1,
+        'title': 'Buy groceries',
+        'description': 'Milk, Cheese, Pizza, Fruit, biscuits',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': 'Learn Python',
+        'description': 'Learn an awesome new programming language',
+        'done': True
+    }
+    ]
+    return render_template("index.html", title="Home", user=user, tasks=tasks)
